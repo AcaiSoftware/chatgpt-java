2ed097a1b49c0103f5f531ef876834e0e2bc6325
Clouke <76826179+Clouke@users.noreply.github.com>
2022-12-09 20:20:14 +0100

    create static `ChatGPTBuilder` builder delegate

--- src/main/java/gg/acai/chatgpt/ChatGPT.java
+++ src/main/java/gg/acai/chatgpt/ChatGPT.java
@@ -1,12 +1,19 @@
 package gg.acai.chatgpt;
 
+import gg.acai.acava.annotated.Use;
+
 /**
  * © Acai Software - All Rights Reserved
  *
  * @author Clouke
  * @since 09.12.2022 18:27
  */
+@Use("Use ChatGPT#newBuilder to create a ChatGPT instance")
 public interface ChatGPT {
+
+    static ChatGPTBuilder newBuilder() {
+        return new ChatGPTBuilder();
+    }
 
     Conversation createConversation();
 
