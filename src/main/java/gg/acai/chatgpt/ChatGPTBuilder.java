8aa9945cde3baed068f273c14290512c175a1a01
Clouke <76826179+Clouke@users.noreply.github.com>
2022-12-09 18:32:57 +0100

    start on `ChatGPT` builder extension

---
+++ src/main/java/gg/acai/chatgpt/ChatGPTBuilder.java
@@ -0,0 +1,42 @@
+package gg.acai.chatgpt;
+
+import gg.acai.acava.Requisites;
+import gg.acai.chatgpt.types.ChatGPTAPI;
+import kong.unirest.Config;
+import kong.unirest.Unirest;
+
+/**
+ * © Acai Software - All Rights Reserved
+ * @author Clouke
+ * @since 09.12.2022 18:20
+ */
+public class ChatGPTBuilder {
+
+    private String sessionToken;
+    private Config config;
+
+    public ChatGPTBuilder config(Config config) {
+        this.config = config;
+        return this;
+    }
+
+    public ChatGPTBuilder sessionToken(String sessionToken) {
+        this.sessionToken = sessionToken;
+        return this;
+    }
+
+    public ChatGPT build() {
+        doBuildProcedure();
+        return new ChatGPTAPI(sessionToken, config);
+    }
+
+    private void doBuildProcedure() {
+        Requisites.requireNonNull(sessionToken, "Session token cannot be null!");
+        this.config = Requisites.applyIfNull(this.config, Unirest.config())
+                .setDefaultHeader("Authorization", "Bearer " + this.sessionToken)
+                .setDefaultHeader("Content-Type", "application/json")
+                .setDefaultHeader("Accept", "application/json")
+                .setDefaultHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36");
+    }
+
+}
