07657dfb55c681205e2c89047e917a76c8bb159f
Clouke <76826179+Clouke@users.noreply.github.com>
2022-12-09 18:01:04 +0100

    implement `Conversation`

--- src/main/java/gg/acai/chatgpt/ChatGPT.java
+++ src/main/java/gg/acai/chatgpt/ChatGPT.java
@@ -2,28 +2,31 @@ package gg.acai.chatgpt;
 
 import gg.acai.acava.scheduler.AsyncPlaceholder;
 import gg.acai.acava.scheduler.Schedulers;
+import gg.acai.chatgpt.request.ChatGPTRequest;
+import gg.acai.chatgpt.request.StandardChatGPTRequest;
 
 /**
  * © Acai Software - All Rights Reserved
  * @author Clouke
  * @since 09.12.2022 17:47
  */
-public class ChatGPT {
+public class ChatGPT implements Conversation {
 
     private static ChatGPT instance;
     private final String sessionToken;
 
     public ChatGPT(String sessionToken) {
         instance = this;
-
         this.sessionToken = sessionToken;
     }
 
-    public AsyncPlaceholder<String> sendMessage(String message) {
+    @Override
+    public AsyncPlaceholder<Response> sendMessage(String message) {
         return Schedulers.supplyAsync(() -> {
-            // what do we send?
+            // @kaiser
+            ChatGPTRequest request = StandardChatGPTRequest.newBuilder()
+                    .build();
             return null;
         });
     }
-
 }
