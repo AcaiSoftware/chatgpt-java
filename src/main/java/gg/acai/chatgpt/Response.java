d7429212ab42a0f84454212d0c99ffd3c3420afa
Clouke <76826179+Clouke@users.noreply.github.com>
2022-12-09 18:01:16 +0100

    create `Response`

---
+++ src/main/java/gg/acai/chatgpt/Response.java
@@ -0,0 +1,14 @@
+package gg.acai.chatgpt;
+
+/**
+ * © Acai Software - All Rights Reserved
+ *
+ * @author Clouke
+ * @since 09.12.2022 17:58
+ */
+@FunctionalInterface
+public interface Response {
+
+    String getMessage();
+
+}
