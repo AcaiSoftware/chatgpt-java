1e75958a4614c52650e5a6277dbe548dd4fbbde9
Kaiserbloo <kaiserbloo@protonmail.com>
2022-12-09 19:58:28 +0000

    .

--- src/main/java/gg/acai/chatgpt/ChatGPTBuilder.java
+++ src/main/java/gg/acai/chatgpt/ChatGPTBuilder.java
@@ -34,10 +34,14 @@ public class ChatGPTBuilder {
     private void doBuildProcedure() {
         Requisites.requireNonNull(sessionToken, "Session token cannot be null!");
         this.config = Requisites.applyIfNull(this.config, Unirest.config())
-                .setObjectMapper(new JacksonObjectMapper())
+                .setObjectMapper(new JacksonObjectMapper() {
+                    @Override
+                    public <T> T readValue(String value, Class<T> valueType) {
+                        return super.readValue(value, valueType);
+                    }
+                })
                 .setDefaultHeader("Content-Type", "application/json")
                 .setDefaultHeader("Accept", "application/json")
                 .setDefaultHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36");
     }
-
 }
