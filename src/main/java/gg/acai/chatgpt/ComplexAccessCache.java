cd9777001e5b7423c6dfc947cdde886fc62907f7
Clouke <76826179+Clouke@users.noreply.github.com>
2022-12-10 20:02:20 +0100

    .

--- src/main/java/gg/acai/chatgpt/ComplexAccessCache.java
+++ src/main/java/gg/acai/chatgpt/ComplexAccessCache.java
@@ -41,7 +41,6 @@ public class ComplexAccessCache {
                     .asObject(AuthSessionEntity.class)
                     .getBody();
 
-
             String accessToken = resp.getAccessToken();
             if (accessToken == null) {
                 throw new RuntimeException("Unauthorized");
