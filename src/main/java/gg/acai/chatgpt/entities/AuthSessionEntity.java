a0352f745e16ad92ac377171055b9587637c3b1d
Kaiserbloo <kaiserbloo@protonmail.com>
2022-12-10 20:44:13 +0000

    Added documentation

--- src/main/java/gg/acai/chatgpt/entities/AuthSessionEntity.java
+++ src/main/java/gg/acai/chatgpt/entities/AuthSessionEntity.java
@@ -12,14 +12,29 @@ public class AuthSessionEntity {
     private String expires;
     private String accessToken;
 
+    /**
+     * Gets the user.
+     *
+     * @return The user
+     */
     public UserEntity getUser() {
         return user;
     }
 
+    /**
+     * Gets the expiry time.
+     *
+     * @return The user
+     */
     public String getExpires() {
         return expires;
     }
 
+    /**
+     * Gets the access token.
+     *
+     * @return The access token
+     */
     public String getAccessToken() {
         return accessToken;
     }
--- src/main/java/gg/acai/chatgpt/entities/MessageEntity.java
+++ src/main/java/gg/acai/chatgpt/entities/MessageEntity.java
@@ -23,52 +23,109 @@ public class MessageEntity implements Message {
     private String role = "user";
     private Content content = new ContentEntity();
 
+    /**
+     * Sets the ID of this message.
+     *
+     * @param id The ID to set
+     * @return This object, for chaining
+     */
     public MessageEntity setId(String id) {
         this.id = id;
         return this;
     }
 
+    /**
+     * Sets the ID of this message using a UUID object.
+     *
+     * @param uuid The UUID to set as the ID
+     * @return This object, for chaining
+     */
     public MessageEntity setId(UUID uuid) {
         this.id = uuid.toString();
         return this;
     }
 
+    /**
+     * Sets the role of this message.
+     *
+     * @param role The role to set
+     * @return This object, for chaining
+     */
     public MessageEntity setRole(String role) {
         this.role = role;
         return this;
     }
 
+    /**
+     * Sets the content of this message.
+     *
+     * @param content The content to set
+     * @return This object, for chaining
+     */
     public MessageEntity setContent(Content content) {
         this.content = content;
         return this;
     }
 
+    /**
+     * Sets the content type of this message.
+     *
+     * @param content_type The content type to set
+     * @return This object, for chaining
+     */
     public MessageEntity setContentType(String content_type) {
         this.content.setContentType(content_type);
         return this;
     }
 
+    /**
+     * Sets the parts of the content of this message.
+     *
+     * @param parts The list of parts to set
+     * @return This object, for chaining
+     */
     public MessageEntity setParts(List<String> parts) {
         this.content.setParts(parts);
         return this;
     }
 
+    /**
+     * Adds one or more parts to the content of this message.
+     *
+     * @param parts The parts to add
+     * @return This object, for chaining
+     */
     @Required
     public MessageEntity setParts(String... parts) {
         this.content.setParts(parts);
         return this;
     }
 
+    /**
+     * Gets the ID of this message.
+     *
+     * @return The ID of this message
+     */
     @Override
     public String getId() {
         return id;
     }
 
+    /**
+     * Gets the role of this message.
+     *
+     * @return The role of this message
+     */
     @Override
     public String getRole() {
         return role;
     }
 
+    /**
+     * Gets the content of this message.
+     *
+     * @return The content of this message
+     */
     @Override
     public Content getContent() {
         return content;
