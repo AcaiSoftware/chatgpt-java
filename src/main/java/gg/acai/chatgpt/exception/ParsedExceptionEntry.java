e035e5ec5b1e8362aa242e766739728c151a5741
Kaiserbloo <kaiserbloo@protonmail.com>
2022-12-10 23:34:22 +0000

    released version 1.0.0

--- src/main/java/gg/acai/chatgpt/exception/ParsedExceptionEntry.java
+++
@@ -1,9 +0,0 @@
-package gg.acai.chatgpt.exception;
-
-/**
- * Represents an entry in the parsed exception.
- *
- * @author Clouke
- * @since 10.12.2022 20:33
- */
-public record ParsedExceptionEntry(String exceptionKeywords, Class<? extends Exception> exceptionClass) {}
