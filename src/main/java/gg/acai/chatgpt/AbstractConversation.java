7298f79da3dc0dbf0fc8874234ff7b153ecde9cb
Clouke <76826179+Clouke@users.noreply.github.com>
2022-12-10 20:01:17 +0100

    move listener to `EventSourceHandler`

--- src/main/java/gg/acai/chatgpt/AbstractConversation.java
+++ src/main/java/gg/acai/chatgpt/AbstractConversation.java
@@ -1,28 +1,20 @@
 package gg.acai.chatgpt;
 
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
 import gg.acai.acava.scheduler.AsyncPlaceholder;
 import gg.acai.acava.scheduler.Schedulers;
-import gg.acai.chatgpt.entities.ResponseEntity;
+import gg.acai.chatgpt.okhttp.EventSourceHandler;
 import gg.acai.chatgpt.request.ChatGPTRequest;
-import kong.unirest.HttpResponse;
-import kong.unirest.Unirest;
-import okhttp3.*;
-import okhttp3.sse.EventSource;
-import okhttp3.sse.EventSourceListener;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
 import okhttp3.sse.EventSources;
 
 import java.io.IOException;
-import java.lang.reflect.Type;
-import java.net.Socket;
-import java.net.SocketException;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
@@ -34,11 +26,6 @@ import java.util.concurrent.TimeUnit;
 public class AbstractConversation implements Conversation {
 
     private static final MediaType JSON = MediaType.get("application/json");
-    private static final Gson gson = new GsonBuilder()
-            .setPrettyPrinting()
-            .create();
-
-    private static final Type type = new TypeToken<ChatGPTRequest>(){}.getType();
     private static final OkHttpClient client = new OkHttpClient.Builder()
             .connectTimeout(60, TimeUnit.SECONDS)
             .writeTimeout(60, TimeUnit.SECONDS)
@@ -46,10 +33,11 @@ public class AbstractConversation implements Conversation {
             .build();
 
     private final UUID uuid;
-    private String lastMessage;
+    private final EventSourceHandler handler;
 
     public AbstractConversation(UUID uuid) {
         this.uuid = uuid;
+        this.handler = new EventSourceHandler();
     }
 
     @Override
@@ -67,12 +55,13 @@ public class AbstractConversation implements Conversation {
         request.setConversationId(uuid);
         ObjectMapper mapper = new ObjectMapper();
 
-        RequestBody b = null;
+        RequestBody b;
         try {
             b = RequestBody.create(mapper.writeValueAsString(request), JSON);
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
+
         Request req = new okhttp3.Request.Builder()
                 .header("Authorization", "Bearer " + ChatGPTAPI.getInstance().getAccessToken())
                 .header("Accept", "text/event-stream")
@@ -81,63 +70,26 @@ public class AbstractConversation implements Conversation {
                 .post(b)
                 .build();
 
-        EventSourceListener listener = new EventSourceListener() {
-            @Override
-            public void onOpen(EventSource eventSource, okhttp3.Response response) {
-                System.out.println("Open");
-            }
-
-            @Override
-            public void onEvent(EventSource eventSource, String id, String type, String data) {
-                //System.out.println(eventSource + " " + id + " " + type + " " + data);
-                try {
-                    ResponseEntity eventResp = mapper.readValue(data, ResponseEntity.class);
-                    System.out.println(eventResp.getMessage().getContent().getParts());
-                } catch (JsonProcessingException e) {
-                    if (data.contains("[DONE]")) {
-                        eventSource.cancel();
-                        System.out.println("Done :D :D");
-                    }
-                }
-            }
-
-            @Override
-            public void onClosed(EventSource eventSource) {
-                System.out.println("Closed");
-            }
-
-            @Override
-            public void onFailure(EventSource eventSource, Throwable t, okhttp3.Response response) {
-                if (t instanceof SocketException)
-                    return;
-                throw new RuntimeException(t);
-            }
-        };
-
-        final EventSource eventSource = EventSources.createFactory(client).newEventSource(req, listener);
-
-
-        String s = null;
-        okhttp3.Response res = null;
+        EventSources.createFactory(client).newEventSource(req, handler);
+        okhttp3.Response res;
         try {
             res = client.newCall(req).execute();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
         ResponseBody body = res.body();
         try {
             return () -> {
                 try {
-                    return body.string();
+                    return Objects.requireNonNull(body, "Body is null").string();
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
             };
-        } catch (Exception e) {
-            e.printStackTrace();
+        } finally {
+            res.close();
         }
-
-        return null;
     }
 
 
@@ -151,7 +103,9 @@ public class AbstractConversation implements Conversation {
     }
 
     @Override
-    public Optional<Response> getFullResponse() {
-        return Optional.empty();
+    public Conversation setStreamResponseListener(StreamResponseListener streamResponseListener) {
+        this.handler.setListener(streamResponseListener);
+        return this;
     }
+
 }
